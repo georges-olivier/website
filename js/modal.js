b4bada36fede16ae5e90a3cbda863660af7494fb
Christian Lund <clund@stilmark.com>
2023-02-25 18:44:24 +0100

    view image

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,29 @@
 let modal;
 
-function openModal(attr = []) {
+function openModal(img) {
+
+	let imgWidth = img[0].naturalWidth;
+	let imgHeight = img[0].naturalHeight;
+	let ratio = 4/3;
+	let max = window.innerHeight * 0.95;
+
+	let modalWidth = (imgWidth > imgHeight) ? max:max / ratio;
+	let modalHeight = (imgWidth < imgHeight) ? max:max / ratio;
+	let padding = (modalWidth == max) ? '':'';
+
 	// Prevent scrolling
 	$('body').addClass('no-scroll');
 	$('body').bind('touchmove', function(e){e.preventDefault()})
 
 	// Show overlay
 	let overlay = $('#overlay');
-	overlay.css('display', 'flex');
+	overlay.css({display: 'flex', top: $(window).scrollTop()});
 
 	// Append modal
 	modal = $('<div>', {class: 'modal'});
+	modal.append($('<img>', {src: img.attr('src')}).css({width: modalWidth + 'px', height: modalHeight + 'px', padding: '3rem'}));
 	overlay.append(modal);
-	console.log('open');
+
 }
 
 function closeModal() {
@@ -20,9 +31,9 @@ function closeModal() {
 	// Enable scrolling
 	$('body').removeClass('no-scroll');
 	$('body').unbind('touchmove')
-
-	modal.remove();
-	console.log('close');
+	if (modal.length) {
+		modal.remove();
+	}
 }
 
 $(function() {
