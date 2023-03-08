34de8f23e7522b4490c1d4712ebfa3fb1cf4b470
Christian Lund <clund@stilmark.com>
2023-03-08 21:24:12 +0100

    Track href

--- js/track.js
+++ js/track.js
@@ -1,7 +1,16 @@
-$(function() {
+function trackPageView() {
 	$.ajax({
 		method: 'POST',
 		url: 'https://api.georgesolivier.art/pageview',
-		data: { uri: $(location).attr('pathname'), useragent: navigator.userAgent}
+		data: { uri: $(location).attr('href'), viewport: $(window).width() + ',' + $(window).height()}
+	});
+}
+
+$(function() {
+	trackPageView();
+
+	$(window).on('hashchange', function() {
+		trackPageView();
+		// let path = $(location).attr('hash');
 	});
 });
