96764094d11e6f2a4aa5bd330a9b9a351ffc4b8c
Christian Lund <clund@stilmark.com>
2023-09-02 20:58:33 +0200

    Share Twitter

--- js/modal.js
+++ js/modal.js
@@ -23,15 +23,46 @@ function openModal(img) {
 	$('#images img[current=true]').removeAttr('current');
 	img.attr('current', true);
 
+	let newline = "\n\n";
+
+	// Share options
+	let shareTwitter = $('<a>', {class: 'button'}).append(
+		$('<i>', {class: 'fa-brands fa-x-twitter'})
+	).on('click', function(e) {
+		e.stopPropagation();
+		window.open('https://twitter.com/share?'+serialize({
+			text: img.attr('imgtitle')+' — from Georges Olivier\'s '+img.attr('imgTheme')+' collection.'+newline+
+			'https://showcase.georgesolivier.art/image/'+img.attr('slug')+newline+
+			'#'+img.attr('imgTheme')+' #art #artworks #artgallery'
+		}), 'social', 'status=no,menubar=no,width=550,height=850');
+	});
+
+	let shareFacebook = $('<a>', {class: 'button'}).append(
+		$('<i>', {class: 'fa-brands fa-facebook-f'})
+	);
+
+	let social = $('<div>').append(
+		shareTwitter,
+		shareFacebook
+	);
+	social.css({position: 'absolute', bottom: 0, left: modalWidth + 15});
+
 	// Append modal
 	modal = $('<div>', {class: 'modal'});
-	modal.append($('<img>', {class: 'preview', src: img.attr('src'), imgSeq: img.attr('imgSeq')}).css({width: modalWidth + 'px', height: modalHeight + 'px', padding: '3rem'}));
+	modal.append(
+		$('<img>', {
+			class: 'preview',
+			src: img.attr('src'),
+			imgSeq: img.attr('imgSeq')}).css({width: modalWidth + 'px', height: modalHeight + 'px', padding: '3rem'
+		}),
+		social
+	);
 	overlay.append(modal);
 }
 
 function closeModal() {
 	$('head title').text('Georges Olivier');
-	window.location.hash = '';
+	history.pushState(null,null,'#');
 	$('#overlay').fadeOut('fast');
 	// Enable scrolling
 	$('body').removeClass('no-scroll');
@@ -40,6 +71,16 @@ function closeModal() {
 		modal.remove();
 		modal = false;
 	}
+}
+
+serialize = function(obj) {
+	var str = [];
+	for (var p in obj) {
+		if (obj.hasOwnProperty(p)) {
+			str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+		}
+	}
+	return str.join("&");
 }
 
 $(function() {
